b59704ab8798ca14b40f5916a2905f979c100569
MagdalenaOkuniewska <magdaok3@gmail.com>
2026-09-10 18:54:29 +0200

    Fix test imports

--- src/users/tests/test_views.py
+++ src/users/tests/test_views.py
@@ -1,4 +1,5 @@
 from unittest.mock import patch
+
 from django.contrib.auth import get_user_model
 from django.contrib.messages import get_messages
 from django.test import TestCase, override_settings
